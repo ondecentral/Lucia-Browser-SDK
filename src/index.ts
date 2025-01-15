2f3ee920dcad7d127f5d691c489697c68f330eb2
Dmitry Yako <dmtyak@gmail.com>
2025-01-16 07:00:55 +1300

    feat: update init to return the instance

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,45 @@
 import LuciaSDKClass from './core';
 import { Config, SDK } from './types';
 
-const LuciaSDK: SDK = {
-  init: (config: Config) => {
-    if (typeof window === 'undefined') return;
+let instance: LuciaSDKClass | null = null;
 
-    const userConfig = config || {};
-
-    const instance = new LuciaSDKClass(userConfig);
-    LuciaSDK.userInfo = instance.userInfo;
-    LuciaSDK.pageView = instance.pageView;
-    LuciaSDK.trackConversion = instance.trackConversion;
-    LuciaSDK.buttonClick = instance.buttonClick;
-    LuciaSDK.sendWalletInfo = instance.sendWalletInfo;
-    LuciaSDK.checkMetaMaskConnection = instance.checkMetaMaskConnection;
-  },
+const ensureInitialized = () => {
+  if (!instance) {
+    throw new Error('LuciaSDK not initialized. Please call LuciaSDK.init() first');
+  }
+  return instance;
 };
+
+const LuciaSDK: SDK = {
+  init: async (config: Config) => {
+    if (typeof window === 'undefined') {
+      throw new Error('LuciaSDK requires a browser environment');
+    }
+
+    if (instance) {
+      // eslint-disable-next-line no-console
+      console.warn('LuciaSDK is already initialized');
+      return instance;
+    }
+
+    if (!config?.apiKey) {
+      throw new Error('apiKey is required in config');
+    }
+
+    instance = new LuciaSDKClass(config);
+    await instance.init();
+    return instance;
+  },
+  userInfo: async (...args) => ensureInitialized().userInfo(...args),
+  pageView: async (...args) => ensureInitialized().pageView(...args),
+  trackConversion: async (...args) => ensureInitialized().trackConversion(...args),
+  buttonClick: async (...args) => ensureInitialized().buttonClick(...args),
+  sendWalletInfo: async (...args) => ensureInitialized().sendWalletInfo(...args),
+  checkMetaMaskConnection: () => ensureInitialized().checkMetaMaskConnection(),
+};
+
+if (typeof window !== 'undefined') {
+  (window as any).LuciaSDK = LuciaSDK;
+}
 
 export default LuciaSDK;
