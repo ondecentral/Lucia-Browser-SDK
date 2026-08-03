75779f756af4f49d7998c08fce2c317f1a882bd1
Dmitry Yako <dmtyak@gmail.com>
2026-08-03 13:51:07 +1200

    chore: point SDK at api.luciaprotocol.com and https staging host

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const SERVER_URL = 'https://api.clickinsights.xyz';
-export const TEST_SERVER_URL = 'http://52.22.177.60/fk';
+export const SERVER_URL = 'https://api.luciaprotocol.com';
+export const TEST_SERVER_URL = 'https://staging.api.clickinsights.xyz';
